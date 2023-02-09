738b53f6338dceca1375700df803859e8b3b5558
SirStyxyy <irfan.indra202@gmail.com>
2023-02-09 15:51:29 +0800

    ye

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ class App{
 		this.assetsPath = './assets/';
         
 		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 500 );
-		this.camera.position.set( 0, 1.6, 0 );
+		this.camera.position.set( 0, 2.5, 0 );
         
         this.dolly = new THREE.Object3D(  );
-        this.dolly.position.set(0, 0, 10);
+        this.dolly.position.set(0, 10, 20);
         this.dolly.add( this.camera );
         this.dummyCam = new THREE.Object3D();
         this.camera.add( this.dummyCam );
